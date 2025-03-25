6090a6ff516f195ad312353dbeb57f1188cb15d0
TheUnholyPear <ryanbain458@hotmail.com>
2025-03-25 20:15:08 +0000

    Initial Commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
     root: 'app',
     base: '/cs408-visualiser/', // repo name goes here!
+    build: {
+        outDir: 'dist'
+    }
 })
